12eed4b4ab6f7c80b75c1a498d95ceba112674a3
MotasemK <mo3tasem66@gmail.com>
2021-06-23 15:04:21 +0300

    arrow-function part 1

--- advanced/arrow-function.js
+++ advanced/arrow-function.js
@@ -1,5 +1,33 @@
-const square = () => {
+const square = (num) => num * num
+
+const squareLong = (num) => {
     return num * num
 }
 
 console.log(square(5)) // Test 
+
+const people = [{
+    name: 'Andrew',
+    age:28
+},{
+    name: 'Motasem',
+    age: 24
+},{
+    name: 'majd',
+    age:22
+}]
+
+const under30 = people.filter((person) => person.age < 25)
+
+console.log(under30)
+
+// Challenge :
+// 1. Find the person with age 22.
+// 2. Print that persons name.
+
+// const age22 = people.filter((person) => person.age === 22)
+// age22.forEach((person) => console.log(person.name))
+
+// this can be summarize to
+const person = people.find((person) => person.age === 22)
+console.log(person.name)
