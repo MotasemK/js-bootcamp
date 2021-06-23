79472bac278175241f726e3ad1a1633be0b65c1a
MotasemK <mo3tasem66@gmail.com>
2021-06-23 22:25:28 +0300

    Setting up the Prototype Object

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,12 +1,27 @@
 // silly Challenge
 
 const Hangman = function(word, remainingGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c','e']
+}
+
+Hangman.prototype.getPuzzel = function(){
+    let puzzel = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' '){
+            puzzel += letter
+        }else{
+            puzzel += '*'
+        }
+    })
+
+    return puzzel
 }
 
 const game1 = new Hangman('cat', 2)
-console.log(game1)
+console.log(game1.getPuzzel())
 
 const game2 = new Hangman('new jersey', 4)
-console.log(game2)
+console.log(game2.getPuzzel())
--- oop/person.js
+++ oop/person.js
@@ -1,17 +1,40 @@
-const Person = function (firstName, lastName, age) { // we dont use arrow function because it dont bind (this)
-    // console.log(this) // referces to a new empty object
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-}
+// Prototypal Inheritance.
 
-const me = new Person('Motasem', 'Khalefa' , 24)    
 // constructor function : initialize the data for our object
 // new : generate a new empty object for this new instance then
 // it gives us access to that empty object in a constructor function
 // via this value
 
-console.log(me)
+const Person = function (firstName, lastName, age, likes = []) { // we dont use arrow function because it dont bind (this)
+    // console.log(this) // referces to a new empty object
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+}
+
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+    
+    this.likes.forEach((like) => { // Here we must use an arrow function to access (this) value from a parent function. because arrow function dont bind (this) value and if we use regular function it will bind (this) as an undefined value.
+        bio += ` ${this.firstName} likes ${like}`
+    })
+
+    return bio
+}
+
+// Person.prototype.location = 'Amman'this will be shared between each instance.
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const me = new Person('Motasem', 'Khalefa' , 24, ['Swimming', 'Travelling'])    
+
+me.setName('Majd Hussein')
+console.log(me.getBio())
 
 const person2 = new Person('fayez', 'alraheel', 25)
-console.log(person2)
+console.log(person2.getBio())
