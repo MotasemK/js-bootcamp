8820a51871c0ceb0f1d7675739e25478ac96f74b
MotasemK <mo3tasem66@gmail.com>
2021-06-23 17:43:42 +0300

    Type coercion

---
+++ advanced/type-coercion.js
@@ -0,0 +1,13 @@
+// Type coercion - a string, a number, a boolean
+
+// a string
+console.log('5' + 5)
+
+// a number
+console.log('5' - 5)
+
+const value = true + 12 // true converts to 1 and false converts to 0
+const type = typeof value
+
+console.log(type)
+console.log(value)
