a14608448bbd635bfe9fe1b38e38726a1ed665eb
MotasemK <mo3tasem66@gmail.com>
2021-06-24 17:53:37 +0300

    Primitives-Objects part 2

--- hangman/app.js
+++ hangman/app.js
@@ -2,17 +2,19 @@
 // if it does great if not we continue up to prototype chain using that internal
 // prototype property. 
 
-// searching chain for objects : product --> object.prototype --> null
+// const { stringify } = require("uuid")
 
+// searching chain for objects : product --> object.prototype --> null
+/*
 const product = {
     name: 'Influence'
 }
-
+*/
 // method : hasOwnProperty .. returns true if the property exists in the
 // object or false if its not.
 
 // Check mdn hasOwnPropery in Google.
-console.log(product.hasOwnProperty('hasOwnProperty'))
+//console.log(product.hasOwnProperty('hasOwnProperty'))
 
 /*
 Ask about this why fires error???! we are chaining inside the product to its prototype
@@ -22,22 +24,48 @@ console.log(product.prototype.hasOwnProperty('hasOwnProperty'))
 Answer : browsers know prototype as __proto__ .. proof(try to console.log(any object))
 */
 
-console.log(product)
+//console.log(product)
 
 // We can add a new methods to the object prototype !! :D :D :D lol and we can override the default process of already exists functions :D:D
 
-Object.prototype.someNewMethod = () => 'This is the new function'
-console.log(product.someNewMethod())
+/* Object.prototype.someNewMethod = () => 'This is the new function'
+console.log(product.someNewMethod()) */
+/*
+BEHIND THE SCENE JS MAKES OBJECTS LIKE THAT.
+CONST PERSON = NEW OBJECT()
+PERSON.NAME = "MOTASEM"
+CONSOLE.LOG(PERSON)
 
-// behind the scene JS makes objects like that.
-// const person = new Object()
-// person.name = "Motasem"
-// console.log(person)
+OR 
 
-// or 
+ CONST PERSON = NEW OBJECT({
+    NAME : 'MOTASEM'
+})
+console.log(person)
+*/
 
-// const person = new Object({
-//     name : 'Motasem'
-// })
-// console.log(person)
+/*-------------------------------------------------------------
+-- Primitive value: string, number, boolean, null, undefined. (((is a value that doesn't have property))).
+Any thing out of this list is an object.
 
+Searching chain.
+Explaining what happend behind the scene
+
+object: myObject --> Object.prototype --> null.
+Array: myArray --> Array.prototype --> Object.prototype --> null
+Function: myFunc --> Function.prototype --> Object.prototype --> null
+String: myString --> string.prototype --> Object.prototype --> null
+Number: myNumber --> Number.prototype --> Object.prototype --> null
+Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+*/
+
+/*const team = ['Luke', 'Cage']
+console.log(team.hasOwnProperty('filter'))*/ // array dont have filter method but array.prototype have.
+
+// const product = 'Computer'
+// console.log(product.split())
+// primitives have object wrapper when we call split method JS behind the scene converts the string to an object
+
+const product = new String('phone')
+console.log(product)
+// if we declare it like that we could explore all the methods contains in the proto.
